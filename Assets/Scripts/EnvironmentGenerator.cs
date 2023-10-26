0e13712be0d62bd8892cd6701edfb26f016f2933
danillque <Danieel.gusev@gmail.com>
2023-10-26 15:17:20 +0200

    Added the random selection.

--- Assets/Scripts/EnvironmentGenerator.cs
+++ Assets/Scripts/EnvironmentGenerator.cs
@@ -35,10 +35,40 @@ public class EnvironmentGenerator : MonoBehaviour
         // Your code for Exercise 1.1 part 3.) here
     }
 
+    bool isEmpty<T>(List<T> list)
+    {
+        return list.Count == 0;
+    }
+
+    #nullable enable
+    GameObject? getRandomGameObjectFrom(List<GameObject> list)
+    {
+        if (isEmpty(list))
+        {
+            return null;
+        }
+
+        var random = new System.Random();
+
+        int randomIndex = random.Next(0, list.Count);
+
+        GameObject randomGameObject = list[randomIndex];
+
+        return randomGameObject;
+    }
+
     void GenerateEnvironment()
     {
         // Your code for Exercise 1.1 part 1.) here
-        StartCoroutine(ResolveCollisions());
+
+        var randomPrefab = getRandomGameObjectFrom(environmentPrefabs);
+
+        if (!randomPrefab)
+        {
+            throw new System.Exception("The given list of prefabs is empty.");
+        }
+
+     StartCoroutine(ResolveCollisions());
     }
 
     IEnumerator ResolveCollisions()
