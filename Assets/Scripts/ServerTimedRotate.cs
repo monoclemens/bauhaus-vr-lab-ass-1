4da47536dc07561b9202cf623ceb644589395ab6
danillque <Danieel.gusev@gmail.com>
2023-11-08 15:32:46 +0100

    Prettified ServerTimedRotate

--- Assets/Scripts/ServerTimedRotate.cs
+++ Assets/Scripts/ServerTimedRotate.cs
@@ -6,20 +6,23 @@ public class ServerTimedRotate : NetworkBehaviour
     public float degreesPerSecondX = 0;
     public float degreesPerSecondY = 20;
     public float degreesPerSecondZ = 0;
-    
+
     // Update is called once per frame
     void Update()
     {
         if (!IsServer)
             return;
 
-        
-        
-
         // Your code for Exercise 1.4 here 
-        Vector3 rotation = new Vector3(degreesPerSecondX, degreesPerSecondY, degreesPerSecondZ) * Time.deltaTime ;
-        transform.Rotate(rotation, Space.World);
+        Vector3 rotation = new(
+            degreesPerSecondX, 
+            degreesPerSecondY, 
+            degreesPerSecondZ);
+
+        // Ensure that frame rates don't affect the rotation speed by including deltatime in the equation.
+        Vector3 normalizedRotation = rotation * Time.deltaTime;
+        
+        // Around the world, baby!
+        transform.Rotate(normalizedRotation, Space.World);
     }
-
-
 }
