4408c15957e4ad1dc1b6dee634b885bf7d72dba1
danillque <Danieel.gusev@gmail.com>
2023-11-23 18:43:06 +0100

    Finished 2.6 of Assignment 2.

--- Assets/Assignment2 - Navigation/Scripts/SteeringNavigation.cs
+++ Assets/Assignment2 - Navigation/Scripts/SteeringNavigation.cs
@@ -10,6 +10,7 @@ public class SteeringNavigation : MonoBehaviour
     public Transform navigationOrigin;
     public Transform steeringHand;
 
+    // TODO: What is this variable for? We don't need it for the Update.
     public float moveSpeed = 2f;
     private float moveThreshhold = 0.05f;
 
@@ -26,6 +27,26 @@ public class SteeringNavigation : MonoBehaviour
     void Update()
     {
         float steeringInput = steeringAction.action.ReadValue<float>();
-        // Exercise 2.6 Steering Navigation
+
+        /************************************
+         * Exercise 2.6 Steering Navigation *
+         ************************************/
+
+        // If the input is not greater than the threshold, just stop the update.
+        if (steeringInput <= moveThreshhold)
+        {
+            return;
+        }
+
+        // Get the delta speed so the movement starts smoothly.
+        float deltaSpeed = steeringInput - moveThreshhold;
+
+        transform.position +=
+            // Use the speed a.k.a the force on the steering hand's controller's input.
+            deltaSpeed
+            // Include the time since the last frame so the movement is smooth.
+            * Time.deltaTime
+            // Move into the forward direction of the steering hand.
+            * steeringHand.forward;
     }
 }
