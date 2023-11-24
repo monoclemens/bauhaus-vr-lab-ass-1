d40e9f3250941973755a63ccf61625080d53015f
danillque <Danieel.gusev@gmail.com>
2023-11-24 17:27:02 +0100

    implemented hitpoint display and previewavatar related functionalities, TELEPORT IS NOT DONE

--- Assets/Assignment2 - Navigation/Scripts/TeleportNavigation.cs
+++ Assets/Assignment2 - Navigation/Scripts/TeleportNavigation.cs
@@ -20,6 +20,8 @@ public class TeleportNavigation : MonoBehaviour
 
     public float rayLength = 10.0f;
     private bool rayIsActive = false;
+    //added to activate the rotation selection for the previewavatar to start
+    public bool previewAvatarPlaced = false;
 
     public XRInteractorLineVisual lineVisual;
     private readonly float rayActivationThreshhold = 0.01f;
@@ -45,6 +47,7 @@ public class TeleportNavigation : MonoBehaviour
 
         bool isValueGreaterThanRayThreshold = teleportActionValue > rayActivationThreshhold;
         bool isValueGreaterThanTeleportThreshold = teleportActionValue > teleportActivationThreshhold;
+        
 
         /**
          * If the value is greater than the ray threshold, the ray should be active and vice versa.
@@ -61,33 +64,34 @@ public class TeleportNavigation : MonoBehaviour
          * If the ray is active but we're not adjusting the teleport yet, 
          * show the hitpoint where the ray hits the layer mask.
          */
-        if (rayIsActive && !isValueGreaterThanTeleportThreshold)
+        if (rayIsActive)
         {
             Debug.Log("Showing the ray...");
 
-            Vector3 origin = lineVisual.transform.localPosition;
-            Vector3 direction = lineVisual.transform.rotation.eulerAngles;
+            Vector3 hitPosition = SetHitPoint();
 
-            Debug.Log("Calculating a hitpoint with the layer mask...");
-
-            bool isHittingLayerMask = Physics.Raycast(origin, direction, out RaycastHit hitInfo, rayLength, groundLayerMask);
-
-            /** 
-             * Put the hitpoint where the ray collided with the ground layer mask.
-             * If there is no collision, deactivate the hitpoint so it doesn't float anywhere needlessly.
-             */
-            if (isHittingLayerMask)
+            if (isValueGreaterThanTeleportThreshold)
             {
-                Debug.Log("Hit found: " + hitInfo.ToString());
 
-                hitpoint.transform.position = hitInfo.point;
+                ShowPreviewAvatar(hitPosition);
             }
             else
             {
-                Debug.Log("No hit found.");
+               
+                previewAvatar.SetActive(false);
+                previewAvatarPlaced = false;
 
-                hitpoint.SetActive(false);
             }
+
+            
+
+
+        }
+        //need to deactivate when ray is inactive cause it floats needlessly otherwise
+        else {
+           
+           DeactivateHitPoint();
+            
         }
 
         // Exercise 2.8 Teleport Navigation
@@ -99,13 +103,102 @@ public class TeleportNavigation : MonoBehaviour
         // }
     }
 
+    //sets the hit point and returns where it is for the preview to be set 
+    //not sure if it is the prettiest way to do it
+    private Vector3 SetHitPoint()
+    {
+        /** 
+        * Put the hitpoint where the ray collided with the ground layer mask.
+        * If there is no collision, deactivate the hitpoint so it doesn't float anywhere needlessly.
+        */
+        Vector3 origin = hand.position;
+        Vector3 direction = hand.forward;
 
+        Debug.Log("Calculating a hitpoint with the layer mask...");
+        bool isHittingLayerMask = Physics.Raycast(origin, direction, out RaycastHit hitInfo, rayLength, groundLayerMask);
+
+        if (isHittingLayerMask)
+        {
+
+            ActivateHitPoint();
+            hitpoint.transform.position = hitInfo.point;
+        }
+        else
+        {
+            Debug.Log("No hit found.");
+            DeactivateHitPoint();
+        }
+
+        return hitInfo.point;
+    }
+
+    private void ShowPreviewAvatar(Vector3 hitPosition)
+    {
+       
+            //place the preview avatar if not previously placed
+        if (!previewAvatarPlaced)
+        {
+            Vector3 previewAvatarPosition = hitPosition;
+            previewAvatarPosition.y = head.position.y;
+            previewAvatar.SetActive(true);
+            previewAvatar.transform.position = previewAvatarPosition;
+            // Set the initial rotation to match the head object
+            previewAvatar.transform.rotation = head.rotation;
+            previewAvatarPlaced = true;
+        }
+        else
+        {
+            // Calculate the rotation to face towards the hitpoint
+            Quaternion lookRotation = Quaternion.LookRotation(previewAvatar.transform.position - hitPosition, Vector3.up);
+
+            // Lock rotation to only rotate around the y-axis
+            Quaternion yRotationOnly = Quaternion.Euler(0f, lookRotation.eulerAngles.y, 0f);
+            previewAvatar.transform.rotation = yRotationOnly;
+        }
+
+
+    
+        
+    }
+    //doesn't work properly yet
+    //TODO: where should the function be placed is the question
+    private void PerformTeleport()
+    {
+        transform.position = previewAvatar.transform.position;
+        transform.rotation = previewAvatar.transform.rotation;
+    }
     private void ToggleRay() => ChangeRayVisibility(!rayIsActive);
 
     private void ChangeRayVisibility(bool isVisible)
     {
         rayIsActive = isVisible;
         lineVisual.enabled = isVisible;
-        hitpoint.SetActive(isVisible);
+        
+    }
+    //prettier looking activation setters
+    private void DeactivateHitPoint()
+    {
+        hitpoint.SetActive(false);
+    }
+    private void ActivateHitPoint()
+    {
+        hitpoint.SetActive(true);
     }
 }
+
+
+/*lock the preview avatar in place as soon as we exceed the teleport threshold
+                if (isValueGreaterThanTeleportThreshold && !previewAvatarPlaced)
+                {
+                    Vector3 previewAvatarPosition = hitInfo.point;
+                    previewAvatarPosition.y = head.position.y;
+                    previewAvatar.SetActive(true);
+                    previewAvatar.transform.position = previewAvatarPosition;
+                    previewAvatar.SetActive(false);
+                    previewAvatarPlaced = true;
+                }
+                else if (!isValueGreaterThanTeleportThreshold)
+                {
+                    previewAvatarPlaced = false;
+
+                }*/
