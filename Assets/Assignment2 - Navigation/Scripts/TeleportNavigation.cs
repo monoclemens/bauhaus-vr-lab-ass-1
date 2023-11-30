05df840d8b3e97c3fafaee5814ac6cac57145c09
danillque <Danieel.gusev@gmail.com>
2023-11-30 14:58:30 +0100

    feat: Started adjusting the code to the new enum.

--- Assets/Assignment2 - Navigation/Scripts/TeleportNavigation.cs
+++ Assets/Assignment2 - Navigation/Scripts/TeleportNavigation.cs
@@ -40,13 +40,19 @@ public class TeleportNavigation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        HideAll();
+    }
+
+    // This method just hides anything of relevance.
+    private void HideAll()
+    {
         lineVisual.enabled = false;
         hitpoint.SetActive(false);
         previewAvatar.SetActive(false);
     }
 
     // This method only figures out in which state the user is.
-    private UserState CalculateNextUserState ()
+    private UserState CalculateNextUserState()
     {
         // Read the current force applied to the teleporting hand.
         float teleportActionValue = teleportAction.action.ReadValue<float>();
@@ -54,21 +60,27 @@ public class TeleportNavigation : MonoBehaviour
         bool isValueGreaterThanRayThreshold = teleportActionValue > rayActivationThreshhold;
         bool isValueGreaterThanTeleportThreshold = teleportActionValue > teleportActivationThreshhold;
 
+        var (isHittingLayerMask, _) = CalculateHitPointCollision();
+
+        Debug.Log("User state is casting ray: " + (userState == UserState.CastingRay));
+        Debug.Log(isValueGreaterThanTeleportThreshold);
+        Debug.Log(isHittingLayerMask);
+
         // If the user just crossed the ray threshold, we should be casting the ray.
         if (userState == UserState.Idle && isValueGreaterThanRayThreshold)
         {
             return UserState.CastingRay;
         }
-        // If the user crossed the teleport threshold, show the preview avatar.
-        else if (userState == UserState.CastingRay && isValueGreaterThanTeleportThreshold)
+        // If the user crossed the teleport threshold AND if there is a hit point collision, show the preview avatar.
+        else if (userState == UserState.CastingRay && isValueGreaterThanTeleportThreshold && isHittingLayerMask)
         {
             return UserState.AdjustingAvatar;
         }
         // If the user is adjusting the teleport but the force becomes smaller than the teleport threshold, they released the trigger.
-        else if (userState == UserState.AdjustingAvatar && !isValueGreaterThanTeleportThreshold) 
+        else if (userState == UserState.AdjustingAvatar && !isValueGreaterThanTeleportThreshold)
         {
             return UserState.ReleasedTrigger;
-        } 
+        }
         // If the user released the trigger and the force is smaller than the ray threshold, we should go back to idling.
         else if (userState == UserState.ReleasedTrigger && !isValueGreaterThanRayThreshold)
         {
@@ -85,132 +97,126 @@ public class TeleportNavigation : MonoBehaviour
          * Exercise 2.8 *
          ****************/
 
-        UserState nextUserState = CalculateNextUserState();
+        userState = CalculateNextUserState();
 
-
-
-        /**
-         * If the value is greater than the ray threshold, the ray should be active and vice versa.
-         * If that's not the case, toggle the ray on/off.
-         */
-        if (isValueGreaterThanRayThreshold != rayIsActive)
+        switch (userState)
         {
-            Debug.Log("Toggling the ray " + (rayIsActive ? "off..." : "on..."));
+            // If we're idling, hide the ray, the hit point and the preview avatar.
+            case UserState.Idle:
+                HideAll();
 
-            ToggleRay();
-        }
+                break;
+            // If we're casting the ray, activate the ray, but only if it's inactive.
+            // Then, show the hit point where it collides with the layer mask, but only if it does collide with the layer mask.
+            case UserState.CastingRay:
+                ActivateInactiveRay();
 
-        /**
-         * If the ray is active but we're not adjusting the teleport yet, 
-         * show the hitpoint where the ray hits the layer mask.
-         */
-        if (rayIsActive)
-        {
-            Debug.Log("Showing the ray...");
+                ShowCollidingHitPoint();
 
-            Vector3 hitPosition = SetHitPoint();
+                break;
+            // If we're adjusting the avatar, start showing the avatar and change its rotation according to the ray's hit point.
+            // We'll ignore the ray because it should already be active.
+            case UserState.AdjustingAvatar:
+                ShowCollidingHitPoint();
 
-            if (isValueGreaterThanTeleportThreshold)
-            {
+                var (_, hitInfo) = CalculateHitPointCollision();
 
-                ShowPreviewAvatar(hitPosition);
-            }
-            else
-            {
+                if (previewAvatarPlaced)
+                {
+                    SetPreviewAvatar(hitInfo.point);
 
-                previewAvatar.SetActive(false);
+                    previewAvatarPlaced = true;
+                }
+                else
+                {
+                    RotatePreviewAvatar(hitInfo.point);
+                }
+
+                break;
+            /**
+             * If the trigger is released:
+             *      - move the user to the preview avatar's position
+             *      - rotate the user to the preview avatar's rotation
+             *      - reset previewAvatarPlaced
+             *      - hide the ray, hit point and the preview avatar.
+             */
+            case UserState.ReleasedTrigger:
+                PerformTeleport();
+
                 previewAvatarPlaced = false;
 
-            }
+                HideAll();
 
-
-
-
-        }
-        //need to deactivate when ray is inactive cause it floats needlessly otherwise
-        else
-        {
-
-            DeactivateHitPoint();
+                break;
 
         }
-
-        /**
-         * In this case we assume the user released the trigger and we should jump.
-         */
-        if (previewAvatarPlaced && !isValueGreaterThanTeleportThreshold)
-        {
-            Debug.Log("Jumping!");
-
-            PerformTeleport();
-        }
-
-        // Exercise 2.8 Teleport Navigation
-        // implement teleport states and functions like e.g.: SetHitpoint(), ShowPreviewAvatar(), PerformTeleport()
-        // if (...) {
-        // ...
-        // hitpoint.transform.position = ... set hitpoint position
-        // ...
-        // }
     }
 
-    //sets the hit point and returns where it is for the preview to be set 
-    //not sure if it is the prettiest way to do it
-    private Vector3 SetHitPoint()
+    private (bool isHittingLayerMask, RaycastHit hitInfo) CalculateHitPointCollision()
     {
-        /** 
-        * Put the hitpoint where the ray collided with the ground layer mask.
-        * If there is no collision, deactivate the hitpoint so it doesn't float anywhere needlessly.
-        */
+        Debug.Log("Calculating a hitpoint with the layer mask...");
+
         Vector3 origin = hand.position;
         Vector3 direction = hand.forward;
 
-        Debug.Log("Calculating a hitpoint with the layer mask...");
-        bool isHittingLayerMask = Physics.Raycast(origin, direction, out RaycastHit hitInfo, rayLength, groundLayerMask);
+        bool isHittingLayerMask = Physics.Raycast(
+            origin,
+            direction,
+            out RaycastHit hitInfo,
+            rayLength,
+            groundLayerMask
+        );
 
         if (isHittingLayerMask)
         {
+            Debug.Log("Hit point found!");
+        }
+        else
+        {
+            Debug.Log("No hit point found.");
+        }
+
+        return (isHittingLayerMask, hitInfo);
+    }
+
+    private void ShowCollidingHitPoint()
+    {
+        var (isHittingLayerMask, hitInfo) = CalculateHitPointCollision();
+
+        if (isHittingLayerMask)
+        {
+            hitpoint.transform.position = hitInfo.point;
 
             ActivateHitPoint();
-            hitpoint.transform.position = hitInfo.point;
         }
         else
         {
-            Debug.Log("No hit found.");
             DeactivateHitPoint();
         }
-
-        return hitInfo.point;
     }
 
-    private void ShowPreviewAvatar(Vector3 hitPosition)
+    private void SetPreviewAvatar(Vector3 position)
     {
+        Vector3 previewAvatarPosition = position;
+        previewAvatarPosition.y = head.position.y;
+        previewAvatar.SetActive(true);
+        previewAvatar.transform.position = previewAvatarPosition;
 
-        //place the preview avatar if not previously placed
-        if (!previewAvatarPlaced)
-        {
-            Vector3 previewAvatarPosition = hitPosition;
-            previewAvatarPosition.y = head.position.y;
-            previewAvatar.SetActive(true);
-            previewAvatar.transform.position = previewAvatarPosition;
-            // Set the initial rotation to match the head object
-            previewAvatar.transform.rotation = head.rotation;
-            previewAvatarPlaced = true;
-        }
-        else
-        {
-            // Calculate the rotation to face towards the hitpoint
-            Quaternion lookRotation = Quaternion.LookRotation(previewAvatar.transform.position - hitPosition, Vector3.up);
-
-            // Lock rotation to only rotate around the y-axis
-            Quaternion yRotationOnly = Quaternion.Euler(0f, lookRotation.eulerAngles.y, 0f);
-            previewAvatar.transform.rotation = yRotationOnly;
-        }
-
-
-
-
+        // Set the initial rotation to match the head object
+        previewAvatar.transform.rotation = head.rotation;
     }
+
+    private void RotatePreviewAvatar(Vector3 hitPointPosition)
+    {
+        // Calculate the rotation to face towards the hitpoint
+        Quaternion lookRotation = Quaternion.LookRotation(previewAvatar.transform.position - hitPointPosition, Vector3.up);
+
+        // Lock rotation to only rotate around the y-axis
+        Quaternion yRotationOnly = Quaternion.Euler(0f, lookRotation.eulerAngles.y, 0f);
+
+        previewAvatar.transform.rotation = yRotationOnly;
+    }
+
     //doesn't work properly yet
     //TODO: where should the function be placed is the question
     private void PerformTeleport()
@@ -218,14 +224,21 @@ public class TeleportNavigation : MonoBehaviour
         transform.position = previewAvatar.transform.position;
         transform.rotation = previewAvatar.transform.rotation;
     }
-    private void ToggleRay() => ChangeRayVisibility(!rayIsActive);
 
     private void ChangeRayVisibility(bool isVisible)
     {
         rayIsActive = isVisible;
         lineVisual.enabled = isVisible;
-
     }
+
+    // This method activates the ray only if it is inactive.
+    private void ActivateInactiveRay()
+    {
+        if (rayIsActive && lineVisual.enabled) return;
+
+        ChangeRayVisibility(true);
+    }
+
     //prettier looking activation setters
     private void DeactivateHitPoint()
     {
