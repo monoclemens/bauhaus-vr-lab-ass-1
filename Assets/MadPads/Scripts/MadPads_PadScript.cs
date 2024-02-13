40d935b8c8aae147b2249c8e9145de216c7d826d
danillque <Danieel.gusev@gmail.com>
2024-02-13 18:51:40 +0100

    feat: Added logic to the pad script for handling all interactions with the peds. Needs testing!

--- Assets/MadPads/Scripts/MadPads_PadScript.cs
+++ Assets/MadPads/Scripts/MadPads_PadScript.cs
@@ -1,18 +1,187 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class MadPads_PadScript : MonoBehaviour
+/**
+ * Hi Cem!
+ * 
+ * I'm not sure if we need to distribute the sound, too. If the sounds are set in the lobby,
+ * every client should have the sound upon Start. If not, we might need to distribute it.
+ * 
+ * Besides, I'm not sure if the pad is automatically the spatial source of the sound. 
+ * It might, because I set the Spatial Blend of the Pad Prefab to 1.
+ * 
+ * Anyway, important network variables for you are "sound" and "color".
+ */
+
+public class MadPads_PadScript : NetworkBehaviour
 {
+    public GameObject pad; 
+    public UnityEvent onTouch;
+    public UnityEvent onLeave;
+    private Color _initialColor;
+
+    // A distributed variable for the sound. This way we can change and distribute it at runtime.
+    public NetworkVariable<AudioSource> sound = new(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    
+    // Distribute the triangle color, too, to show the color of the pad.
+    public NetworkVariable<Color> color = new(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    // This bool needs to be shared so all clients know if the a pad is being touched.
+    private readonly NetworkVariable<bool> isTouched = new(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    // A networked variable to keep track of who touches a pad.
+    private readonly NetworkVariable<GameObject> touchingPlayer = new(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    // A singleton for the renderer with a corresponding getter.
+    private Renderer _renderer;
+    public Renderer Renderer
+    {
+        get
+        {
+            if (_renderer == null)
+            {
+                _renderer = gameObject.GetComponent<Renderer>();
+            }
+
+            Debug.Assert(_renderer != null, "Could not find Renderer of pad " + gameObject.name + "!");
+
+            return _renderer;
+        }
+    }
+
+    // Another singleton for the interactive material.
+    private Material _interactiveMaterial;
+    public Material InteractiveMaterial
+    {
+        get
+        {
+            if (_interactiveMaterial == null)
+            {
+                // The element at index 1 is the interactive material.
+                _interactiveMaterial = Renderer.materials[1];
+            }
+
+            Debug.Assert(_interactiveMaterial != null, "Could not find interactive material of pad " + gameObject.name + "!");
+
+            return _interactiveMaterial;
+        }
+    }
+
+    // Another singleton for the triangle material.
+    private Material _triangleMaterial;
+    public Material TriangleMaterial
+    {
+        get
+        {
+            if (_triangleMaterial == null)
+            {
+                // The element at index 2 is the triangle material.
+                _triangleMaterial = Renderer.materials[2];
+            }
+
+            Debug.Assert(_triangleMaterial != null, "Could not find triangle material of pad " + gameObject.name + "!");
+
+            return _triangleMaterial;
+        }
+    }
+
+    // This can be done locally, because the interaction color is always the same.
+    private void TogglePadColor()
+    {
+        InteractiveMaterial.color = InteractiveMaterial.color == _initialColor
+            ? Color.blue
+            : _initialColor;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Pad " + gameObject.name + " checking in!");
+
+        // Here we distribute a bool, this might be unnecessary and we could probably just initialite it with false.
+        isTouched.Value = false;
+
+        // Locally keep track of the intial color. No need to distribute.
+        _initialColor = InteractiveMaterial.color;
+
+        /**
+         * Attach a listener to the color network variable.
+         * TODO: We might need to "subtract" it on destroy. Check!
+         */
+        color.OnValueChanged += OnColorChanged;
+    }
+
+    // The listener changing the triangle color.
+    public void OnColorChanged (Color previous, Color current)
+    {
+        TriangleMaterial.color = current;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    // This method will be executed when a player collides with the collider of the pad.
+    private void OnTriggerEnter(Collider playerCollider)
+    {
+        // Early return if it is already being touched.
+        if (isTouched.Value) return;
+
+        // Let everyone know who touches the pad.
+        touchingPlayer.Value = playerCollider.gameObject;
+
+        // Set the bool now so all clients learn about the change.
+        isTouched.Value = true;
+
+        // And trigger the sound, color etc. on every client.
+        PlayOnAllClientsClientRpc();
+    }
+
+    [ClientRpc]
+    void PlayOnAllClientsClientRpc()
+    {
+        // Locally change the pads color for everyone.
+        TogglePadColor();
+
+        // TODO: Check if this needs to happen: https://www.youtube.com/watch?v=lPPa9y_czlE
+        onTouch.Invoke();
+
+        // If there is a sound, play it now.
+        if (sound.Value != null)
+        {
+            sound.Value.Play();
+        }
+    }
+
+    // This method will be executed when a player collides with the collider of the pad.
+    private void OnTriggerExit(Collider playerCollider)
+    {
+        // Early return if the one releasing is not the one currently touching it.
+        if (playerCollider.gameObject != touchingPlayer.Value) return;
+
+        // Let everyone know that the pad is not being touched anymore.
+        isTouched.Value = false;
+
+        StopOnAllClientsClientRpc();
+    }
+
+    [ClientRpc]
+    void StopOnAllClientsClientRpc()
+    {
+        // Locally change the pads color for everyone.
+        TogglePadColor();
+
+        // TODO: Check if this needs to happen: https://www.youtube.com/watch?v=lPPa9y_czlE
+        onLeave.Invoke();
+
+        // If there is a sound playing, stop it now.
+        if (sound.Value != null
+            && sound.Value.isPlaying)
+        {
+            sound.Value.Stop();
+        }
     }
 }
