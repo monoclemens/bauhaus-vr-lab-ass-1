2047d6f2cbb28e77f6fd34496ab41450bba510a6
danillque <Danieel.gusev@gmail.com>
2024-02-13 16:58:23 +0100

    feat: Gave all Pads a unique name. Also added a script for the pad prefab.

---
+++ Assets/MadPads/Scripts/MadPads_PadScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MadPads_PadScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Pad " + gameObject.name + " checking in!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
